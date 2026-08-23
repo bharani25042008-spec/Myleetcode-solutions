410dcea971be0a0f934c575edc664b83e3a06a74
bharani25042008-spec <bharani25042008@gmail.com>
2026-08-23 19:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (19.59%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,43 +1,36 @@
 class Solution {
 public:
-void func(int i,int j,int c,vector<vector<int>>&grid,vector<vector<int>>&vis,int src){
-      if(i>=grid.size()||j>=grid[0].size()){
-         return;
-      }
-      if(i<0||j<0){
-         return;
-      }
-      if(vis[i][j]==1){
-           return;
-      }
-      if(grid[i][j]==src){
-         grid[i][j]=c;
-      }
-      vis[i][j]=1;
-      if(i-1>=0){
-      if(grid[i-1][j]==src)
-        func(i-1,j,c,grid,vis,src);
-      }
-      if(i+1<grid.size()){
-      if(grid[i+1][j]==src)
-      func(i+1,j,c,grid,vis,src);
-      }
-      if(j+1<grid[0].size()){
-      if(grid[i][j+1]==src)
-      func(i,j+1,c,grid,vis,src);
-      }
-      if(j-1>=0){
-        if(grid[i][j-1]==src)
-      func(i,j-1,c,grid,vis,src);
-      }
-      vis[i][j]=0;
-}
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+          queue<pair<int,int>>q;
           int m=image.size();
           int n=image[0].size();
           vector<vector<int>>vis(m,vector<int>(n,0));
+          vis[sr][sc]=1;
           int src=image[sr][sc];
-          func(sr,sc,color,image,vis,src);
+          image[sr][sc] = color;
+          q.push({sr,sc});
+          int dr[]={-1,1,0,0};
+          int dc[]={0,0,-1,1};
+          while(!q.empty()){
+             auto [r,c]=q.front();
+             q.pop();
+             for(int k=0;k<4;k++){
+                 int nr=r+dr[k];
+                 int nc=c+dc[k];
+                 if(nr>=image.size()||nc>=image[0].size()||nr<0||nc<0){
+                    continue;
+                 }
+                 if(vis[nr][nc]==1){
+                    continue;
+                 }
+                 if(image[nr][nc]!=src){
+                   continue;
+                 }
+                    vis[nr][nc]=1;
+                    image[nr][nc]=color;
+                    q.push({nr,nc});
+             }
+          }
           return image;
     }
 };
