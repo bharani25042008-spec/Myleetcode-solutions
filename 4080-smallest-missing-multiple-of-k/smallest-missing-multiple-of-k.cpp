2b22b11dd7e89edbfedaee6630baea24657f5d96
bharani25042008-spec <bharani25042008@gmail.com>
2026-08-25 18:30:20 +0530

    Time: 2 ms (42.52%) | Memory: 26 MB (24.36%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -3,7 +3,7 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
         set<int>st(nums.begin(),nums.end());
         vector<int>ans;
-        for(int i=1;i<=1000;i++){
+        for(int i=1;i<=500;i++){
              if(i%k==0){
                  ans.push_back(i);
              }
