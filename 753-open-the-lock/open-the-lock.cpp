a451028ba73364071212680f46849f705dfee74e
bharani25042008-spec <bharani25042008@gmail.com>
2026-08-26 21:41:36 +0530

    Time: 383 ms (12.42%) | Memory: 44.6 MB (24.13%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -3,7 +3,9 @@ public:
     int openLock(vector<string>& deadends, string target) {
         queue<pair<string,int>>q;
         set<string>st(deadends.begin(),deadends.end());
-        if(st.count("0000")) return -1;
+        if(st.count("0000")) return -1;//edge yenna namma starting position eh deadend ah irundha we can't go anywhere just return -1;
+        //genetic mutation and word ladder pattern with the same bfs idea just adding +1 and -1 are the costs of the state
+        //yet 0-1 bfs intuition was a nightmare for me :(
         q.push({"0000",0});
         st.insert("0000");
         while(!q.empty()){
@@ -25,7 +27,9 @@ public:
                  st.insert(s);
                  q.push({s,dist+1});
                  }
-                 s[i]=temp;
+                 s[i]=temp;//this line is the most important guy as he is the parent for the rest of his child 
+                 // i need more grid dp and digit dp problems to solve the bfs if hell as boring and tough in the way of intuiton  how the competitive programmers are able to crack the convex hull with the ease still trying but it is tough though
+                 //let's go and solve some dp problems and gain some confidence 
               }
         }
         return -1;
