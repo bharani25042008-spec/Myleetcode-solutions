d87936d1945853652f0be6aeb28093c38bd7faea
bharani25042008-spec <bharani25042008@gmail.com>
2026-09-04 11:48:47 +0530

    Time: 3 ms (65.32%) | Memory: 178.7 MB (63.16%) - LeetSync

---
+++ 4375-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPenalty(int period, vector<int>& lights, vector<int>& arrivalTime) {
+          int maxi=*max_element(lights.begin(),lights.end());
+          int idx=0;
+          for(int i=0;i<lights.size();i++){
+             if(lights[i]==maxi){
+                   idx=i;
+                   break;
+             }
+          }
+          int ans=INT_MIN;
+          for(int i=0;i<arrivalTime.size();i++){
+            int val=0;
+               int r=arrivalTime[i]%period;
+               if(r<maxi){
+                  val=0;
+               }else{
+                 val=period-r;
+               }
+               ans=max(ans,val);
+          }
+          return ans;
+    }
+};
