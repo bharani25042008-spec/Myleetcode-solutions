bbb44df29c38e7c4896cbdd100481902c8fb0bdd
bharani25042008-spec <bharani25042008@gmail.com>
2026-09-04 18:24:29 +0530

    Time: 775 ms (43.35%) | Memory: 198.6 MB (48.35%) - LeetSync

---
+++ 3926-minimum-cost-path-with-alternating-directions-iii/minimum-cost-path-with-alternating-directions-iii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    long long minCost(int m, int n, vector<vector<int>>& penalty) {
+        vector<vector<vector<long long>>>dist(m,vector<vector<long long>>(n,vector<long long>(2,LLONG_MAX)));
+        dist[0][0][0]=1;
+        priority_queue<tuple<long long,int,int,int>,vector<tuple<long long,int,int,int>>,greater<tuple<long long,int,int,int>>>q;
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+        q.push({1,0,0,0});
+        while(!q.empty()){
+              auto[d,r,c,p]=q.top();
+              q.pop();
+              if(r==m-1&&c==n-1){
+                 return d;
+              }
+            int np = (p + 1) % 2;
+            long long waitCost = d + penalty[r][c];
+            if (waitCost < dist[r][c][np]) {
+
+                dist[r][c][np] = waitCost;
+
+                q.push({
+                    waitCost,
+                    r,
+                    c,
+                    np
+                });
+            }
+              for(int i=0;i<4;i++){
+                 int nr=r+dr[i];
+                 int nc=c+dc[i];
+                 long long cost=(nr+1)*(nc+1);
+                 if(nr>=m||nc>=n||nr<0||nc<0) continue;
+                 if((p+1)%2==1){
+                    if(dr[i]==0&&dc[i]==-1){
+                        cost+=penalty[r][c];
+                    }
+                    if(dr[i]==-1&&dc[i]==0){
+                       cost+=penalty[r][c];
+                 }
+                 }else{
+                   if(dr[i]==0&&dc[i]==1){
+                    cost+=penalty[r][c];
+                 }
+                   if(dr[i]==1&&dc[i]==0){
+                     cost+=penalty[r][c];
+                   }
+               }
+               if(cost+d<dist[nr][nc][(p+1)%2]){
+                 dist[nr][nc][(p+1)%2]=cost+d;
+                 q.push({cost+d,nr,nc,(p+1)%2});
+               }
+        }
+    }
+    return 0;
+    }
+};
