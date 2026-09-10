0947941a66846007cf512fbdf25b6915993561f1
bharani25042008-spec <bharani25042008@gmail.com>
2026-09-10 23:09:26 +0530

    Time: 1042 ms (16.76%) | Memory: 15.4 MB (30.27%) - LeetSync

--- 4095-number-of-balanced-integers-in-a-range/number-of-balanced-integers-in-a-range.cpp
+++ 4095-number-of-balanced-integers-in-a-range/number-of-balanced-integers-in-a-range.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 long  long dp[17][2][2][100][100];
 // shit i forgot to add the cnt in the base case 
+//digit dp is easy
 int c1(int idx,int n){
      if(idx%2==0){
           return n;
