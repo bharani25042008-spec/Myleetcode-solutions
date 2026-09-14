913e06b3f70aed4c8512e7513de04b088ffafed5
bharani25042008-spec <bharani25042008@gmail.com>
2026-09-14 21:14:47 +0530

    Time: 51 ms (72.84%) | Memory: 85.5 MB (31.69%) - LeetSync

--- 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
@@ -3,38 +3,35 @@ public:
 vector<vector<pair<int,int>>>adj;
     Graph(int n, vector<vector<int>>& edges) {
          adj.resize(n);
-          //vector<vector<pair<int,int>>>adj(n);
-          //this->adj=adj;
-          for(auto it:edges){
+         for(auto it:edges){
                adj[it[0]].push_back({it[1],it[2]});
-          }
+         }
     }
     
     void addEdge(vector<int> edge) {
-           adj[edge[0]].push_back({edge[1],edge[2]});
+         adj[edge[0]].push_back({edge[1],edge[2]});
     }
     
     int shortestPath(int node1, int node2) {
-           int n=adj.size();
-           priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
-           q.push({0,node1});
-           vector<int>dist(n,INT_MAX);
-           dist[node1]=0;
-           while(!q.empty()){
-               auto[d,node]=q.top();
-               q.pop();
-               if(node==node2){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        vector<int>dist(adj.size(),INT_MAX);
+        dist[node1]=0;
+        q.push({0,node1});
+        while(!q.empty()){
+              auto[d,node]=q.top();
+              q.pop();
+              if(node==node2){
                     return d;
-               }
-               for(auto nei:adj[node]){
-                     int nd=d+nei.second;
-                     if(nd<dist[nei.first]){
-                         dist[nei.first]=nd;
-                         q.push({nd,nei.first});
-                     }
-               }
-           }
-           return -1;
+              }
+              for(auto nei:adj[node]){
+                  int nd=d+nei.second;
+                  if(nd<dist[nei.first]){
+                     dist[nei.first]=nd;
+                     q.push({nd,nei.first});
+                  }
+              }
+        }
+        return -1;
     }
 };
 
