45507a250a58c44900fad72785274b1f11e7083f
bharani25042008-spec <bharani25042008@gmail.com>
2026-09-15 11:52:28 +0530

    Time: 617 ms (7.71%) | Memory: 406 MB (5.08%) - LeetSync

---
+++ 4092-finish-time-of-tasks-i/finish-time-of-tasks-i.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+const int mod=1e9+7;
+__int128 func(int node,vector<vector<int>>&adj,vector<int>&b,map<int,__int128>&mp){
+      if(mp.find(node)!=mp.end()){
+           return mp[node];
+      }
+      if(adj[node].size()==0){
+             return b[node];
+      }
+    __int128 maxi=LLONG_MIN;
+    __int128 mini=LLONG_MAX;
+      for(auto it:adj[node]){
+              __int128 val=func(it,adj,b,mp);
+                maxi=max(maxi,val);
+                mini=min(mini,val);
+      }
+      return mp[node]=(maxi+((maxi-mini)+(__int128)b[node]));
+}
+    long long finishTime(int n, vector<vector<int>>& edges, vector<int>& baseTime) {
+          vector<vector<int>>adj(n);
+          
+          map<int,__int128>mp;
+          for(auto it:edges)
+          {
+              adj[it[0]].push_back({it[1]});
+          }
+          for(int i=n-1;i>=0;i--){
+                if(mp.find(i)!=mp.end()){
+                      continue;
+                }else{
+                    mp[i]=func(i,adj,baseTime,mp);
+                }
+          }
+        //   for(auto it:mp){
+        //      cout<<it.first<<":"<<it.second<<' ';
+        //   }
+          return mp[0];
+    }
+};
