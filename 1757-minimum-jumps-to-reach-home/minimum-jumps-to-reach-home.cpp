fda773aa6b78a4915dcf3c1883461605852f07c1
bharani25042008-spec <bharani25042008@gmail.com>
2026-09-19 17:39:46 +0530

    Time: 198 ms (6.54%) | Memory: 90 MB (6.08%) - LeetSync

---
+++ 1757-minimum-jumps-to-reach-home/minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
+           queue<tuple<int,int,int>>q;
+        //   int n=*max_element(forbidden.begin(),forbidden.end());
+        //   n=max(n,a);
+        //   n=max(n,b);
+        //   n=max(n,x);
+          set<int>st(forbidden.begin(),forbidden.end());
+          q.push({0,0,2});
+          vector<vector<int>>vis(10000,vector<int>(3,0));
+          vis[0][2]=1;
+          while(!q.empty()){
+              auto[d,node,dir]=q.front();
+              q.pop();
+              if(node==x) return d;
+              if(dir==2){
+                  if(node+a<10000){
+                      if(vis[node+a][1]==0&&st.find(node+a)==st.end()){
+                         vis[node+a][1]=1;
+                         q.push({d+1,node+a,1});
+                      }
+                  }
+                  if(node-b>=0){
+                     if(vis[node-b][0]==0&&st.find(node-b)==st.end()){
+                         vis[node-b][0]=1;
+                         q.push({d+1,node-b,0});
+                     }
+                  }
+              }
+              if(dir!=0){
+                  if(node-b>=0){
+                     if(vis[node-b][0]==0&&st.find(node-b)==st.end()){
+                         vis[node-b][0]=1;
+                         q.push({d+1,node-b,0});
+                     }
+                  }
+              }
+              if(node+a<10000){
+                 if(vis[node+a][1]==0&&st.find(node+a)==st.end()){
+                     vis[node+a][1]=1;
+                     q.push({d+1,node+a,1});
+                 }
+              }
+          }
+          return -1;
+    }
+};
